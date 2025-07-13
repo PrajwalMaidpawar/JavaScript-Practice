ec37c0cb79ab5585a995732a8fcbfb2f41920bea
Prajwal Maidpawar <maidpawarp@gmail.com>
2025-07-13 20:50:20 +0530

    Practice More

---
+++ Practice 4/p4.js
@@ -0,0 +1,9 @@
+let g = document.getElementById("Google").addEventListener("click", function() {
+    window.open("https://www.google.com", "_blank");
+});
+let f = document.getElementById("FB").addEventListener("click", function(){
+    window.open("https://www.facebook.com", "_blank");
+})
+let t = document.getElementById("Twitter").addEventListener("click", function(){
+    window.open("https://www.twitter.com", "_blank");
+})
